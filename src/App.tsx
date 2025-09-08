44cdfcac812f396d04c049d1b9e2f0d86a123ceb
Urmila Parte <urmilaparte23@navgurukul.org>
2025-09-08 20:42:23 +0530

    again push the login and auth changes

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import { AuthProvider } from "@/hooks/useAuth";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 import PartnerPage from "./pages/Partner";
 import AdminPage from "./pages/Admin";
-import students from "./pages/students";
+import Students from "./pages/students";
 import Donor from "./pages/Donor";
 import Campus from "./pages/Campus";
 import CampusDetail from "./pages/CampusDetail";
